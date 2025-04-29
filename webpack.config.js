c19e02d0c1013e5b5b9933f27860bfefbeeee077
EvenOff-first <maksim.ferber@gmail.com>
2025-04-29 15:05:50 +0300

    update webpack for correct build

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,17 @@ const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: "development",
     devtool: "eval-source-map",
     entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        assetModuleFilename: "assets/[hash][ext][query]"
+    },
     resolve: {
         extensions: [".ts", ".js"],
     },
@@ -14,34 +20,40 @@ module.exports = {
         rules: [
             {
                 test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.json$/,
-                type: 'json',
+                use: "ts-loader",
                 exclude: /node_modules/,
             },
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: ["style-loader", "css-loader"],
             },
             {
-                test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                type: 'asset/resource',
+                test: /\.(png|jpg|jpeg|gif|svg|mp3|ogg|wav)$/i,
+                type: "asset/resource",
             },
         ],
     },
     plugins: [
-        new CleanWebpackPlugin({
-            root: path.resolve(__dirname, "./dist")
-        }),
+        new CleanWebpackPlugin(),
         new webpack.DefinePlugin({
             CANVAS_RENDERER: JSON.stringify(true),
-            WEBGL_RENDERER: JSON.stringify(true)
+            WEBGL_RENDERER: JSON.stringify(true),
         }),
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
+            template: "./src/index.html",
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "public/assets", to: "assets" }
+            ]
         })
-    ]
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        compress: true,
+        port: 3000,
+        open: true,
+    },
 };
